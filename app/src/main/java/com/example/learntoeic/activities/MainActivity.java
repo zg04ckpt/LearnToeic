c350c5e461e34559e2c5d5e1abe58fcd35385997
zg04ckpt <nguyenhc424@gmail.com>
2024-03-01 13:59:18 +0700

    add learn activities and fix something

--- app/src/main/java/com/example/learntoeic/activities/MainActivity.java
+++ app/src/main/java/com/example/learntoeic/activities/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity implements IUnitListener {
     private UnitAdapter adapter;
     List<Unit> units;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/learntoeic/utilities/Const.java
+++ app/src/main/java/com/example/learntoeic/utilities/Const.java
@@ -8,6 +8,7 @@ public class Const {
     public static final String KEY_UNIT_ID = "key-unit-id";
     public static final String KEY_UNIT_NAME = "key-unit-name";
     public static final String KEY_UNIT_CREATED_TIME = "key-unit-created-time";
+    public static final String KEY_WORD_LIST = "key-word-list";
     public static final String KEY_WORD_EN = "key-word-en";
     public static final String KEY_WORD_VN = "key-word-vn";
     public static final String KEY_WORD_TYPE = "key-word-type";
